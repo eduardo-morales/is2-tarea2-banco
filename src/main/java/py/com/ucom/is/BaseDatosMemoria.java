10c08fec6f148fafa011f8289a3db4eb4dab594b
profesor <carlos@gmail.com>
2022-05-11 16:59:06 -0400

    prueba

--- src/main/java/py/com/ucom/is/BaseDatosMemoria.java
+++ src/main/java/py/com/ucom/is/BaseDatosMemoria.java
@@ -11,47 +11,47 @@ public class BaseDatosMemoria {
 	private static BaseDatosMemoria bd = null;
 	private static ArrayList<Cuenta> cuentaRegistroTabla = new ArrayList<Cuenta>();
 	private static ArrayList<Pago> pagoTabla = new ArrayList<Pago>();
-	
+
 	public static BaseDatosMemoria getInstancia() {
-		if(bd == null) {
-			bd =  new BaseDatosMemoria();
+		if (bd == null) {
+			bd = new BaseDatosMemoria();
 		}
 		return bd;
 	}
-	
-    public int insertarCuentaRegistro(Cuenta registro) {
-    	//validar que no se repita cedula o numero de cuenta
-    	for (Cuenta registroGuardado : cuentaRegistroTabla) {
-			if(registroGuardado.getCedula().equalsIgnoreCase(registro.getCedula())
-					&& registroGuardado.getCuenta().intValue()== registro.getCuenta().intValue()) {
-				
+
+	public int insertarCuentaRegistro(Cuenta registro) {
+		// validar que no se repita cedula o numero de cuenta
+		for (Cuenta registroGuardado : cuentaRegistroTabla) {
+			if (registroGuardado.getCedula().equalsIgnoreCase(registro.getCedula())
+					&& registroGuardado.getCuenta().intValue() == registro.getCuenta().intValue()) {
+
 				return 1000;
 			}
 		}
-    	
-    	cuentaRegistroTabla.add(registro);
-    	return 0;
-    }
-    
-    public ArrayList<Cuenta> getCuentaRegistros() {
-    	return cuentaRegistroTabla;
-    }
-	
-    public int insertarPago(Pago pago) {
-    	Boolean valido = false;
+
+		cuentaRegistroTabla.add(registro);
+		return 0;
+	}
+
+	public ArrayList<Cuenta> getCuentaRegistros() {
+		return cuentaRegistroTabla;
+	}
+
+	public int insertarPago(Pago pago) {
+		Boolean valido = false;
 		for (Cuenta registroGuardado : cuentaRegistroTabla) {
-			if(registroGuardado.getCedula().equalsIgnoreCase(pago.getCedula())
-					&& registroGuardado.getCuenta().intValue()== pago.getCuenta().intValue()) {
-				
+			if (registroGuardado.getCedula().equalsIgnoreCase(pago.getCedula())
+					&& registroGuardado.getCuenta().intValue() == pago.getCuenta().intValue()) {
+
 				valido = true;
 			}
 		}
-		if(!valido) {
+		if (!valido) {
 			return 1000;
-			
+
 		}
-		
-    	pagoTabla.add(pago);
-    	return 0;
-    }
+
+		pagoTabla.add(pago);
+		return 0;
+	}
 }
